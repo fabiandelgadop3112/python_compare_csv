e6208f82e57d2b3d0e0d3834f13232b0448f7247
Fabian Delgado <nfabian.1231@gmail.com>
2020-05-16 20:52:01 -0500

    Comparacion inicial entre registros de dos CSV

--- compare_csv.py
+++ compare_csv.py
@@ -1,8 +1,12 @@
-csv_origin = open("CSV_1.csv", "r")
-print (csv_origin.read() + "\r\n")
+csv_origin = open("CSV_1.csv", "r", encoding='utf-8')
+csv_modified = open("CSV_2.csv", "r", encoding='utf-8')
 
-csv_modified = open("CSV_2.csv", "r")
-print (csv_modified.read())
+list_origin = csv_origin.readlines()
+list_modified = csv_modified.readlines()
 
-
+for line in list_modified:
+    if line in list_origin:
+        print (f'{line} se encuentra en origen')
+    else:
+        print (f'{line} no se encuentra en origen')
 
