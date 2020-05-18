66b5e0f52b8251b3aa238f360f015322ae7f4ce7
Fabian Delgado <nfabian.1231@gmail.com>
2020-05-17 21:55:02 -0500

    Se incluye la decodifcación de UTF-8 a ASCII para comparar los acentos y otros caracteres

--- compare_csv.py
+++ compare_csv.py
@@ -1,11 +1,28 @@
+from unidecode import unidecode
+
 csv_origin = open("CSV_1.csv", "r", encoding='utf-8')
 csv_modified = open("CSV_2.csv", "r", encoding='utf-8')
 
 list_origin = csv_origin.readlines()
 list_modified = csv_modified.readlines()
 
+list_origin_decode = [unidecode(line) for line in list_origin]
+
 for line in list_modified:
     if line in list_origin:
-        print (f'{line} se encuentra en origen')
+        print (f'{line} MATCH without modifications')
+    
+    elif line in list_origin_decode:
+        print (f'{line} MATCH removing some accent marks or Ñ character')
+
+    elif unidecode(line) in list_origin:
+        print (f'{line} MATCH adding some accent marks or Ñ character')
+    
+    elif unidecode(line) in list_origin_decode:
+        print (f'{line} MATCH adding and removing some accent marks or Ñ character')
+
     else:
-        print (f'{line} NO se encuentra en origen')
+        print (f'{line} DOES NOT match') 
+        
+
+    
--- replace_characters.py
+++
@@ -1,4 +0,0 @@
-from unidecode import unidecode
-characters = "áéíóúñ"
-print(f'codificado: {characters}')
-print (f'decodificado: {unidecode(characters)}')
