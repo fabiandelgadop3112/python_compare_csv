b4b0b42587015ea69d56970ac6379aa70c2ca7da
Fabian Delgado <nfabian.1231@gmail.com>
2020-05-18 19:36:31 -0500

    Se genera archivo CSV con los resultados de la comparacion y se agrega log en consola

--- compare_csv.py
+++ compare_csv.py
@@ -1,28 +1,42 @@
 from unidecode import unidecode
 
+line_break = '\n'
 csv_origin = open("CSV_1.csv", "r", encoding='utf-8')
 csv_modified = open("CSV_2.csv", "r", encoding='utf-8')
+out_file = open("results.csv", "w" , encoding='utf-8')
 
 list_origin = csv_origin.readlines()
 list_modified = csv_modified.readlines()
 
 list_origin_decode = [unidecode(line) for line in list_origin]
 
+line_number = 0
+
 for line in list_modified:
+
     if line in list_origin:
-        print (f'{line} MATCH without modifications')
-    
+
+        out_line = f'{line.rstrip(line_break)},MATCH without modifications{line_break}'
+
     elif line in list_origin_decode:
-        print (f'{line} MATCH removing some accent marks or Ñ character')
+
+        out_line =  f'{line.rstrip(line_break)},MATCH removing some accent marks or Ñ character{line_break}'
 
     elif unidecode(line) in list_origin:
-        print (f'{line} MATCH adding some accent marks or Ñ character')
-    
+
+        out_line =  f'{line.rstrip(line_break)},MATCH adding some accent marks or Ñ character{line_break}'
+
     elif unidecode(line) in list_origin_decode:
-        print (f'{line} MATCH adding and removing some accent marks or Ñ character')
+
+        out_line =  f'{line.rstrip(line_break)},MATCH adding and removing some accent marks or Ñ character{line_break}'
 
     else:
-        print (f'{line} DOES NOT match') 
-        
+        out_line =  f'{line.rstrip(line_break)},DOES NOT match{line_break}'
+
+    out_file.write(out_line)
+    print(f'line_number {line_number} succesfully processed')
+    line_number += 1    
+
+print(f'File {out_file.name} was created succesfully')
 
     
